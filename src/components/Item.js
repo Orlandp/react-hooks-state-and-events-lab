727ea518d9c3fdb4f629742e8d5fadf109df8058
pknowledge <jamesblssings@gmail.com>
2024-11-06 01:43:22 +0300

    state abd events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react"; // Make sure to import useState
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false); // State for tracking if item is in cart
+  
+  // Function to toggle item in cart
+  const toggleInCart = () => {
+    setInCart(!inCart);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}> {/* Conditional className */}
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleInCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"} {/* Conditional button text */}
+      </button>
     </li>
   );
 }
